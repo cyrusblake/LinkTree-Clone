bae771d77b5144f289edf9daa695e634b46a8b81
Cyrus Blake <cyrusblake89@gmail.com>
2025-02-24 22:02:42 -0500

    more

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { signInWithGoogle, signOutFromGoogle } from '../lib/firebase';
 import { Link } from 'react-router-dom';
-import './Header.css';
+import './header.css';
 import useAuth from '../lib/useAuth';
 
 export default function Header() { 
